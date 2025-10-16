b19f986a3e524f7293fe1b6312d5cf693047c914
Truong Dang <dangtruong040206@gmail.com>
2025-10-16 17:07:02 +0700

    Thuc hanh JS

---
+++ prc.js
@@ -0,0 +1,42 @@
+// Thực hành khai báo biến //
+
+// Name and Year of Birth //
+var fullName = " Dang Hai Phi Truong";
+var birthDay = 2006;
+
+alert("Practice started");
+
+console.log(fullName);
+console.log(birthDay);
+var soyeuthich = prompt("Nhap so ban thich");
+confirm("Do you finish ?");
+console.log("So yeu thich la:",soyeuthich)
+
+
+//  Ap dung toan tu so //
+
+var now = 2025;
+var year = 2006;
+
+var myAge= now - year;
+console.log("My age:",myAge);
+
+//  Toan tu gan //
+
+var x = 5;
+
+x += 10;
+
+x *= 3;
+
+console.log("Result:", x );
+
+// Prefix and Postfix //
+var num1 = 8;
+var res1 = ++num1;
+
+var num2 = 8;
+var res2 = num2++;
+
+console.log("Res1:",res1)
+console.log("Res2:",res2)
